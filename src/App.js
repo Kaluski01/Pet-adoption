33fae3a1acf1f632612f9da667e8a62cd8fc3e80
Divine <nzedivine55@gmail.com>
2024-02-21 12:21:07 +0100

    made some changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Header folder/home';
 import BasicExample  from './Pages/Header folder/navbar';
@@ -17,52 +17,31 @@ import Storedpets from './Pages/Header folder/Storedpets';
 import Login from './Pages/sign up/Login';
 import Addpet from './Pages/sellerdashboard/Addpet';
 
-// Create a guest to communicate with the host (not used in this code)
-// const storageGuest = createGuest('http://pet-adoption-seven.vercel.app');
+const firebaseConfig = {
+  apiKey: "AIzaSyA8R-6JStMc680KVXXQ_XGSRche5OQUtl4",
+  authDomain: "pet-adoption-project-99a08.firebaseapp.com",
+  projectId: "pet-adoption-project-99a08",
+  storageBucket: "pet-adoption-project-99a08.appspot.com",
+  messagingSenderId: "226601747265",
+  appId: "1:226601747265:web:24326e722ad314f751d7c0",
+  measurementId: "G-WJRCH01VP2"
+};
+
+firebase.initializeApp(firebaseConfig) 
 
 function App() {
   const [pets, setPets] = useState([]);
-
-  // Fetch pet information from local storage on app load
-  useEffect(() => {
-    const handleStorageChange = (event) => {
-      if (event.key === 'pets') {
-        try {
-          const storedPets = event.newValue;
-          if (storedPets) {
-            setPets(JSON.parse(storedPets));
-          }
-        } catch (error) {
-          console.error('Error loading pets from local storage:', error);
-        }
-      }
-    };
-  
-    // Listen for changes in the local storage
-    window.addEventListener('storage', handleStorageChange);
-  
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []); // Empty dependency array to ensure this effect runs only once
-  
 
   // Function to add a new pet
   const addNewPet = (newPet) => {
     // Update the local state
     setPets((prevPets) => [newPet, ...prevPets]);
-
-    // Update the local storage
     try {
       const updatedPets = JSON.stringify([newPet, ...pets]);
       localStorage.setItem('pets', updatedPets);
     } catch (error) {
       console.error('Error updating pets in local storage:', error);
     }
-
-    // Update the remote storage via guest (not used in this code)
-    // storageGuest.setItem('pets', [newPet, ...pets]);
   };
 
   return (
